c1e434527a77a0daf665f92ee9660cb335e3a966
landercor <asprillafabian034@gmail.com>
2026-03-10 21:20:36 -0500

    router: Update auth routes to use centralized auth controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,25 +1,27 @@
 import express from "express";
-import { viewRegister, viewLogin, forgotPassword } from "../controllers/user.controller.js";
+import { register, login, forgotPassword, resetPassword } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.get("/register", (req, res) => {
   res.render("register");
 });
-
-router.post("/register", viewRegister);
+router.post("/register", register);
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
+router.post("/login", login);
 
-router.post("/login", viewLogin);
-
-// new routes for password recovery
+// Password recovery routes
 router.get("/olvido", (req, res) => {
   res.render("olvido");
 });
-
 router.post("/olvido", forgotPassword);
+
+router.get("/nuevaclave", (req, res) => {
+  res.render("nuevaclave");
+});
+router.post("/nuevaclave", resetPassword);
 
 export default router;
